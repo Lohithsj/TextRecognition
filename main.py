3674b4ff2d4209c0656f7a80c594b30bc0cc85e2
lohith <lohithgowdasj@gmail.com>
2022-09-15 19:11:37 +0530

    code snippet

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import pytesseract
 
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread("example2.jpeg")
+img = cv2.imread(r"C:\Users\lohit\PycharmProjects\TextRecognition\Images\example1.jpeg")
 
 text = pytesseract.image_to_string(img)
 print(text)
