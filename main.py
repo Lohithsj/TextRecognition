b935d4f56ba9f36e871b9449fa9665f136ba2015
lohith <lohithgowdasj@gmail.com>
2022-09-16 23:53:49 +0530

    code snippet

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import cv2
 import pytesseract
 import re
 from datetime import datetime
-#from dateutil import parser
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread(r"C:\Users\lohit\PycharmProjects\TextRecognition\Images\billstack.jpg")
+img = cv2.imread(r"C:\Users\lohit\PycharmProjects\TextRecognition\Images\sidecar.jpg")
 
 text = pytesseract.image_to_string(img)
 print(text)
 
-date_match = re.search(r'\d{2}-\d{2}-\d{4}',text)
+date_match = re.search(r'\d{2}/\d{2}/\d{4}',text)
 
-result = datetime.strptime(date_match.group(), '%d-%m-%Y').date()
+result = datetime.strptime(date_match.group(), '%d/%m/%Y').date()
 
 
 print("Extracted date from image is " + str(result))
